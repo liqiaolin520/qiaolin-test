45412e96e0f5067ae1ca9411e4d7981a446826d4
qiaolin <992004863@qq.com>
2018-04-04 15:56:40 +0800

    JavaDoc测试

---
+++ test-javadoc/src/main/java/indi/qiaolin/java/doc/test/JavaDocTest.java
@@ -0,0 +1,24 @@
+package indi.qiaolin.java.doc.test;
+
+/**
+ * 这是一个简单的JavaDoc 案例 
+ * 测试JavaDoc
+ * @author qiaolin
+ * @version 1.0
+ **/
+
+public class JavaDocTest {
+	/**
+	 *  测试成员变量的文档注释
+	 *  @author qiaolin
+	 */
+	public String name;
+	
+	/**
+	 *  这是一个程序的入口
+	 * @param args 命令行参数
+	 */
+	public static void main(String[] args) {
+		
+	}
+}
